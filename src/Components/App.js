b99cf03b39aa70d08c2867f9f723a73772838bd8
ThomasPerez91 <dialldialldiall@gmail.com>
2024-04-05 12:27:00 +0200

    chore: add filters

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,16 @@
 import "../styles/app.css";
 
 import React, { useState, useEffect } from "react";
+
 import TaskList from "./TaskComponents/TaskList";
 import TaskForm from "./TaskComponents/TaskForm";
+import TaskFilter from "./TaskComponents/TaskFilter";
 
 function App() {
   const [tasks, setTasks] = useState(
     JSON.parse(localStorage.getItem("tasks")) || []
   );
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -19,14 +22,56 @@ function App() {
     setTasks([...tasks, newTask]);
   };
 
-  const toggleTask = () => {};
+  const toggleTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, done: !task.done } : task
+      )
+    );
+  };
+
+  const getFilteredTasks = () => {
+    switch (filter) {
+      case "done":
+        return tasks.filter((task) => task.done);
+      case "active":
+        return tasks.filter((task) => !task.done);
+      default:
+        return tasks;
+    }
+  };
 
   return (
     <div className="App">
       <h1>Task List</h1>
       <TaskForm onNewTask={handleNewTask} />
-
-      <TaskList data-cy="task-list" tasks={tasks} toggleTask={toggleTask} />
+      <div>
+        <div className="btn-group">
+          <TaskFilter
+            label="Toutes"
+            setFilter={setFilter}
+            filter="all"
+            dataCy="filter-btn-all"
+          />
+          <TaskFilter
+            label="Non complétées"
+            setFilter={setFilter}
+            filter="active"
+            dataCy="filter-btn-undone"
+          />
+          <TaskFilter
+            label="Complétées"
+            setFilter={setFilter}
+            filter="done"
+            dataCy="filter-btn-done"
+          />
+        </div>
+      </div>
+      <TaskList
+        data-cy="task-list"
+        tasks={getFilteredTasks()}
+        toggleTask={toggleTask}
+      />
     </div>
   );
 }
