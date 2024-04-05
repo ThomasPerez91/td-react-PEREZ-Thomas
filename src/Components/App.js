2d040ef9cab95606016d673e77d352a580d60820
ThomasPerez91 <dialldialldiall@gmail.com>
2024-04-05 11:17:14 +0200

    chore: add Task List component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,17 @@
 import "../styles/app.css";
 
 import React, { useState, useEffect } from "react";
-import TaskItem from "./TaskComponents/TaskItem";
+import TaskList from "./TaskComponents/TaskList";
 
 function App() {
   const textTask = "New Task";
-  const newTask = { id: Date.now(), textTask, done: false };
+  const newTask = [{ id: Date.now(), textTask, done: false }];
 
   const toggleTask = () => {};
 
   return (
     <div className="App">
-      <TaskItem data-cy="task-Items" task={newTask} toggleTask={toggleTask} />
+      <TaskList data-cy="task-list" tasks={newTask} toggleTask={toggleTask} />
     </div>
   );
 }
